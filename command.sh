ce4971adece061ce56041c618cf573802dc91a66
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-25 01:04:39 +0100

    Add tag

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-aws cloudformation update-stack --stack-name DNS --template-body file://cloudformation.yml;
+aws cloudformation update-stack --stack-name DNS --template-body file://cloudformation.yml --tags Key=service,Value=dns;
